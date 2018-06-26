97580fc1b7635128bf044198bcb969b2823b0bc2
Jeff Mace <jmace@vmware.com>
2018-06-26 09:32:13 -0400

    Move some logic into an external Groovy file

---
+++ support/jenkinsMethods.groovy
@@ -0,0 +1,25 @@
+def install() {
+    // Set up Python virtual environment and install pyvcloud. 
+    sh "support/install.sh"
+}
+
+def tox() {
+    // Run tox. 
+    sh "support/tox.sh"
+}
+
+def runSamples(credentialsArray) {
+    withCredentials(credentialsArray) {
+        // Execute samples. 
+        sh "examples/run_examples.sh"
+    }
+}
+
+def runSystemTests(credentialsArray) {
+    withCredentials(credentialsArray) {
+        // Run the default system test list. 
+        sh "system_tests/run_system_tests.sh"
+    }
+}
+
+return this
