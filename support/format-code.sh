a99e1902d4e50bd1af731bba76a9e6d060fdd537
Jeff Mace <jmace@vmware.com>
2018-07-02 15:23:01 -0400

    Revert these scripts to match the upstream versions

--- support/format-code.sh
+++ support/format-code.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
-SHOME=`dirname $0`
-cd $SHOME
-
-SRCROOT=`cd ..; pwd`
-cd $SRCROOT
-
-. ./support/lib.sh
-
-if [ "$PYTHON3_IN_DOCKER" != "0" ]; then
-    run_in_docker support/format-code.sh
-else
-    if [ -z "$VIRTUAL_ENV" ]; then
-        . $PYVCLOUD_VENV_DIR/bin/activate
-    fi
-    
-    yapf -i pyvcloud/vcd/*.py
-    flake8 pyvcloud/vcd/*.py
-fi
+yapf -i pyvcloud/vcd/*.py
+flake8 pyvcloud/vcd/*.py
--- support/publish.sh
+++ support/publish.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
-set -e
 
-SHOME=`dirname $0`
-cd $SHOME
-
-SRCROOT=`cd ..; pwd`
-cd $SRCROOT
-
-. ./support/lib.sh
-
-if [ "$PYTHON3_IN_DOCKER" != "0" ]; then
-    run_in_docker support/publish.sh
-else
-    if [ -z "$VIRTUAL_ENV" ]; then
-        . $PYVCLOUD_VENV_DIR/bin/activate
-    fi
-    
-    python3 setup.py develop
-    python3 setup.py sdist bdist_wheel
-    twine upload dist/*
-fi
+python setup.py develop
+python setup.py sdist bdist_wheel
+twine upload dist/*
